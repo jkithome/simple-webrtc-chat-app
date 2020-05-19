b2cf910e0368d79f8edf87e3b85a1f5a729a147f
fullcycle <dfoderick@gmail.com>
2020-05-18 17:42:59 -0700

    basic chatting to myself

--- src/Chat.js
+++ src/Chat.js
@@ -13,6 +13,7 @@ import { format } from "date-fns";
 import "./App.css";
 import UsersList from "./UsersList";
 import MessageBox from "./MessageBox";
+import Paho from "paho-mqtt"
 
 // Use for remote connections
 const configuration = {
@@ -38,21 +39,60 @@ const Chat = ({ connection, updateConnection, channel, updateChannel }) => {
   const messagesRef = useRef({});
   const [messages, setMessages] = useState({});
 
+  const client = useRef(null)
+
   useEffect(() => {
-    webSocket.current = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
-    webSocket.current.onmessage = message => {
-      const data = JSON.parse(message.data);
-      setSocketMessages(prev => [...prev, data]);
-    };
-    webSocket.current.onclose = () => {
-      webSocket.current.close();
-    };
-    return () => webSocket.current.close();
+    // webSocket.current = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
+    // webSocket.current.onmessage = message => {
+    //   const data = JSON.parse(message.data);
+    //   setSocketMessages(prev => [...prev, data]);
+    // };
+    // webSocket.current.onclose = () => {
+    //   webSocket.current.close();
+    // };
+    // return () => webSocket.current.close();
+
+    webSocket.current = new WebSocket('ws://bitcoinofthings.com:1884');
+
+    const clientID = "bot-demo-ws-" + parseInt(Math.random() * 100);
+    const host = "mqtt.bitcoinofthings.com"
+    const port = "1884"
+    const isSSL = true
+    const usessl = (isSSL && port === "8884")
+    const username = "demo"
+    const password = "demo"
+    client.current = new Paho.Client(host, Number(port), clientID)
+    // Set callback handlers
+    //client.onConnectionLost = onConnectionLost
+    client.current.onMessageArrived = onMessageArrived
+    client.current.connect({ 
+      useSSL: usessl,
+      userName: username,
+      password: password,
+      onSuccess: onConnect
+    })
+
   }, []);
+
+  // Called when the client connects
+function onConnect() {
+  setSocketOpen(true);
+  client.current.subscribe("demo");
+  console.log("subscribed to demo topic")
+}
+
+// Called when a message arrives
+function onMessageArrived(message) {
+  console.log("onMessageArrived: " + message.payloadString);
+  const data = JSON.parse(message.payloadString)
+  setSocketMessages(prev => [...prev, data])
+}
 
   useEffect(() => {
     let data = socketMessages.pop();
     if (data) {
+      console.log("effect data")
+      console.log(data)
       switch (data.type) {
         case "connect":
           setSocketOpen(true);
@@ -76,6 +116,8 @@ const Chat = ({ connection, updateConnection, channel, updateChannel }) => {
           onCandidate(data);
           break;
         default:
+          console.log("other message received")
+          console.log(data)
           break;
       }
     }
@@ -86,14 +128,22 @@ const Chat = ({ connection, updateConnection, channel, updateChannel }) => {
   };
 
   const send = data => {
-    webSocket.current.send(JSON.stringify(data));
+    //webSocket.current.send(JSON.stringify(data));
+    client.current.publish("demo",JSON.stringify(data))
   };
 
   const handleLogin = () => {
     setLoggingIn(true);
+    // todo: broker should do login
+    // for now, in demo mode
+    // users should be array of logged in users
+    // back end will need to maintain
     send({
       type: "login",
-      name
+      name,
+      success: true,
+      message:"This is just a demo",
+      users: [{key:name, userName: name}]
     });
   };
 
@@ -137,6 +187,9 @@ const Chat = ({ connection, updateConnection, channel, updateChannel }) => {
       );
       setIsLoggedIn(true);
       setUsers(loggedIn);
+
+      //setup connection to peer
+      //can we pass webrtc message over mqtt???
       let localConnection = new RTCPeerConnection(configuration);
       //when the browser finds an ice candidate we send it to another peer
       localConnection.onicecandidate = ({ candidate }) => {
@@ -180,37 +233,40 @@ const Chat = ({ connection, updateConnection, channel, updateChannel }) => {
     setConnectedTo(name);
     connectedRef.current = name;
 
-    connection
-      .setRemoteDescription(new RTCSessionDescription(offer))
-      .then(() => connection.createAnswer())
-      .then(answer => connection.setLocalDescription(answer))
-      .then(() =>
+    // connection
+    //   .setRemoteDescription(new RTCSessionDescription(offer))
+    //   .then(() => connection.createAnswer())
+    //   //erroring, not sure what it does
+    //   //.then(answer => connection.setLocalDescription(answer))
+    //   .then(() =>
         send({ type: "answer", answer: connection.localDescription, name })
-      )
-      .catch(e => {
-        console.log({ e });
-        setAlert(
-          <SweetAlert
-            warning
-            confirmBtnBsStyle="danger"
-            title="Failed"
-            onConfirm={closeAlert}
-            onCancel={closeAlert}
-          >
-            An error has occurred.
-          </SweetAlert>
-        );
-      });
-  };
+      // )
+      // .catch(e => {
+      //   console.log({ e });
+      //   setAlert(
+      //     <SweetAlert
+      //       warning
+      //       confirmBtnBsStyle="danger"
+      //       title="Failed"
+      //       onConfirm={closeAlert}
+      //       onCancel={closeAlert}
+      //     >
+      //       There was an error accepting offer.
+      //     </SweetAlert>
+      //   );
+      // });
+  }
 
   //when another user answers to our offer
   const onAnswer = ({ answer }) => {
-    connection.setRemoteDescription(new RTCSessionDescription(answer));
+    console.log('thanks for accepting. you are now connected')
+    //connection.setRemoteDescription(new RTCSessionDescription(answer));
   };
 
   //when we got ice candidate from another user
   const onCandidate = ({ candidate }) => {
-    connection.addIceCandidate(new RTCIceCandidate(candidate));
+    console.log('we dont do ice candidates')
+    //connection.addIceCandidate(new RTCIceCandidate(candidate));
   };
 
   //when a user clicks the send message button
@@ -232,14 +288,17 @@ const Chat = ({ connection, updateConnection, channel, updateChannel }) => {
       messagesRef.current = userMessages;
       setMessages(userMessages);
     }
-    channel.send(JSON.stringify(text));
+    //just send it to group, set up channels later
+    //channel.send(JSON.stringify(text));
+    send(JSON.stringify(text));
     setMessage("");
   };
 
+  //try connecting to user?
   const handleConnection = name => {
-    var dataChannelOptions = {
-      reliable: true
-    };
+    // var dataChannelOptions = {
+    //   reliable: true
+    // };
 
     let dataChannel = connection.createDataChannel("messenger");
 
@@ -282,6 +341,7 @@ const Chat = ({ connection, updateConnection, channel, updateChannel }) => {
   };
 
   const toggleConnection = userName => {
+    console.log("toggleConnection")
     if (connectedRef.current === userName) {
       setConnecting(true);
       setConnectedTo("");
@@ -300,7 +360,7 @@ const Chat = ({ connection, updateConnection, channel, updateChannel }) => {
       {alert}
       <Header as="h2" icon>
         <Icon name="users" />
-        Simple WebRTC Chap App
+        Web Monetization Chat App
       </Header>
       {(socketOpen && (
         <Fragment>
